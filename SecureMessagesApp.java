1af34df310d92577f8f57b30a35d992140d018e1
Pr1st396 <134052125+Pr1st396@users.noreply.github.com>
2026-01-21 14:30:28 +0200

    Add files via upload

---
+++ SecureMessagesApp.java
@@ -0,0 +1,24 @@
+/*auhtor: Lesedi Moukangwe
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package securemessagesapp;
+
+import za.ac.tut.ui.SecureMessagesFrame;
+
+/**
+ *
+ * @author MemaniV
+ */
+public class SecureMessagesApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new SecureMessagesFrame();
+    }
+    
+}
